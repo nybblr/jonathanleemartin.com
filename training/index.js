ffadd705413695fbe990768a19ba8e451c672337
Jonathan Martin <me@nybblr.com>
2019-11-09 00:10:07 -0500

    Add a nice popout menu.

---
+++ training/index.js
@@ -0,0 +1,36 @@
+(global => {
+
+let toggleMenu = document.querySelectorAll('.js-toggle-menu');
+let navbar = document.querySelector('.js-menu');
+let scrollBody = document.querySelector('.js-scroll-body');
+let clickBody = document.body;
+
+let hide = () => {
+  navbar.classList.remove('menu-expanded');
+  scrollBody.classList.remove('body-scroll-lock');
+  clickBody.removeEventListener('click', hide);
+};
+
+let show = () => {
+  navbar.classList.add('menu-expanded');
+  scrollBody.classList.add('body-scroll-lock');
+  setTimeout(() => {
+    clickBody.addEventListener('click', hide);
+  }, 0);
+};
+
+let toggle = () => {
+  let isOpen = navbar.classList.contains('menu-expanded');
+  isOpen ? hide() : show();
+};
+
+let handleToggle = (event) => {
+  event.preventDefault();
+  toggle();
+};
+
+toggleMenu.forEach(button => {
+  button.addEventListener('click', handleToggle);
+});
+
+})(window);
